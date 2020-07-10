7fc2005fd84fb010b8f609572c6891a04b8d7d9c
bibhas05041984 <bibhas.apr5@gmail.com>
2020-07-09 22:57:01 -0400

    * Japan.html: * Index.cshtml: * typed.js: * Indian.cshtml: * sandwich.jpg: * jquery.js: * custom.js: * wow.min.js: * Indian.cshtml.cs: * Group4_Assignment2.csproj: * Samosa2.jpg: * animate.min.css: * bootstrap.min.js: * bootstrap.min.css: * chana-masala.jpg: * font-awesome.min.css: * templatemo-style.css: * img_chardonnay.jpg: * dal-stew-indian.jpg: * jquery.singlePageNav.min.js: * baingan-bharta-stew.jpg: * Idli_Dosa_breakfast.JPG: * roti-naan-whole-wheat.jpg: * north-Indian-lunch-ideas.jpg: * lamb-veggie-kebab-skewers.jpg: * tandoori-chicken-indian-food.jpg:
    * Privacy.cshtml: Committing to new repo

---
+++ Group4_Assignment2/Pages/Indian.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Group4_Assignment2.Pages
+{
+    public class IndianModel : PageModel
+    {
+        private readonly ILogger<IndianModel> _logger;
+
+        public IndianModel(ILogger<IndianModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
